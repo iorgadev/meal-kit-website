e0b80aa9af06a5f2ac406a6855f9e5963e53ce19
iorgadev <2624432+iorgadev@users.noreply.github.com>
2020-10-28 11:57:04 -0400

    form validation progress

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const port = process.env.PORT || 8080;
 //static meals data module file
 const meals = require(__dirname + '/meals.js');
 
+//serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+//express handlebars settings
 app.engine('hbs', hbs({
     extname: "hbs",
     defaultLayout: "main",
@@ -24,6 +27,10 @@ app.engine('hbs', hbs({
     partialsDir: "views/partials"
 }));
 app.set('view engine', 'hbs');
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Routes
 app.get("/", (req,res) => {
@@ -49,6 +56,29 @@ app.get("/on-the-menu", (req,res) => {
         category
     });
 });
+
+app.post("/login", (req,res) =>{
+    let { email, password } = req.body;
+    let errors = {};
+    errors.found = 0;
+
+    if(!email){
+        errors.email = "Please enter a valid email address.";
+        errors.found++;
+    }
+    
+    if(!password){
+        errors.password = "Please enter a password.";
+        errors.found++;
+    }
+
+    if(errors.found > 0)
+        res.json({errors});
+    else {
+        res.json({error: "no errors"});
+    }
+
+})
 
 //make server listen to port specified
 app.listen(port, () => {
